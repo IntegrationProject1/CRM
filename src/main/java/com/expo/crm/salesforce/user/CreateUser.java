450dffb524e64f31aad2578adae0dc4cbfff445f
DragonRouge513 <goethuys.antoine513@outlook.com>
2025-03-28 20:54:40 +0100

    create contact in salesforce + add all file/dir to the map (only function)

---
+++ src/main/java/com/expo/crm/salesforce/user/CreateUser.java
@@ -0,0 +1,27 @@
+package com.expo.crm.salesforce.user;
+
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+
+import io.github.cdimascio.dotenv.Dotenv;
+
+public class CreateContact {
+    public static void createContact(String jsonObject) throws Exception {
+        // Load environment variables
+        Dotenv dotenv = Dotenv.load();
+        String urlApi = "/services/data/v63.0/sobjects/Contact/";
+        String salesforceUrl = dotenv.get("SALESFORCE_URL");
+        HttpClient client = HttpClient.newHttpClient();
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(URI.create(salesforceUrl + urlApi))
+                .POST(HttpRequest.BodyPublishers.ofString(jsonObject))
+                .header("Authorization", "Bearer " + dotenv.get("SALESFORCE_TOKEN"))
+                .header("Content-Type", "application/json")
+                .build();
+        HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
+        System.out.print(response.statusCode() + "\n");
+        System.out.print(response.body() + "\n");
+    }
+}
